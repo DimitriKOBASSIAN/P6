2861ccb2e17fa48fb4560580279d3908b19f71e8
Dimitri Kobassian <d.kobassian@gmail.com>
2024-07-03 16:53:33 +0200

    work ongoing

---
+++ backend/routes/book.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const auth = require('../middleware/auth');
+
+const bookCtrl = require('../controllers/book');
+
+router.get('/', bookCtrl.getAllBooks);
+router.post('/', bookCtrl.createBook);
+router.get('/:id', bookCtrl.getOneBook);
+router.put('/:id', bookCtrl.modifyBook);
+router.delete('/:id', bookCtrl.deleteBook);
+
+module.exports = router;
