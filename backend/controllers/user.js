8a7a049727f10f73d0ed3ba8c01ca9fa3ff7e091
Dimitri Kobassian <d.kobassian@gmail.com>
2024-07-09 13:56:08 +0200

    update on user controller

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -2,30 +2,22 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
 
-const generateToken = (userId) => {
-    return jwt.sign(
-        { userId: userId },
-        'RANDOM_TOKEN',
-        { expiresIn: '24h' }
-    );
-};
-
 
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
-      .then(hash => {
+        .then(hash => {
         const user = new User({
-          email: req.body.email,
-          password: hash
+            email: req.body.email,
+            password: hash
         });
         user.save()
-          .then(() => res.status(201).json({ message: 'User created !' }))
-          .catch(error => res.status(400).json({ error }));
-      })
-      .catch(error => res.status(500).json({ error }));
-  };
+            .then(() => res.status(201).json({ message: 'User created !' }))
+            .catch(error => res.status(400).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }));
+};
 
-  exports.login = (req, res, next) => {
+exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
         .then(user => {
             if (!user) {
@@ -48,4 +40,4 @@ exports.signup = (req, res, next) => {
                 .catch(error => res.status(500).json({ error }));
         })
         .catch(error => res.status(500).json({ error }));
- };
+};
